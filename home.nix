dd1d348978d876537b7c70238d1a47ce5521377b
Manuel Hutter <manuel@hutter.io>
2023-04-13 10:45:34 +0200

    Set up git
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- home.nix
+++ home.nix
@@ -1,10 +1,13 @@
 { config, pkgs, ... }:
 
+let
+  username = "mh";
+in
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "mh";
-  home.homeDirectory = "/home/mh";
+  home.username = username;
+  home.homeDirectory = "/home/${username}";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -14,6 +17,10 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "22.11"; # Please read the comment before changing.
+
+  imports = [
+    ./programs/git.nix
+  ];
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
---
+++ programs/git.nix
@@ -0,0 +1,104 @@
+{ ... }:
+{
+  programs.git = {
+    enable = true;
+
+    userName = "Manuel Hutter";
+    userEmail = "manuel@hutter.io";
+    signing.key = "3D7A6B26F12CD714";
+    signing.signByDefault = true;
+
+    extraConfig = {
+      color = {
+        ui = "auto";
+      };
+      core = {
+        editor = "vim";
+        autocrlf = "input";
+      };
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+      };
+      pull.rebase = true;
+      rebase.autoStash = true;
+      rerere.enabled = 1;
+      init.defaultBranch = "main";
+      diff.sopsdiffer.textconv = "sops -d";
+    };
+
+    aliases = {
+      ci = "commit -v -s";
+      co = "checkout";
+      st = "status";
+      s = "status -s";
+      ol = "log --oneline --graph";
+      lg = "log --oneline --decorate --all --graph";
+      hist = "log --graph --full-history --all --color --pretty=format:'%x1b[33m%h%x09%C(blue)(%ar)%C(reset)%x09%x1b[32m%d%x1b[0m%x20%s%x20%C(dim white)-%x20%an%C(reset)'";
+      dc = "diff --check";
+      tag-dates = "log --tags --simplify-by-decoration --pretty=\"format:%ai %d\"";
+      log-sig = "log --pretty=\"format:%h %G? %aN %s\"";
+      unstage = "reset HEAD";
+      staged = "diff --staged";
+      yolopush = "push --force-with-lease";
+      fix = "commit --fixup";
+      squ = "commit --squash";
+      ri = "rebase -i --autosquash";
+      pp = "pull --prune";
+      brnach = "branch";
+    };
+
+    ignores = [
+      # from https://help.github.com/articles/ignoring-files
+      # Compiled source #
+      ###################
+      "*.class"
+      "*.dll"
+      "*.exe"
+      "*.o"
+      "*.so"
+      "gin-bin"
+
+      # Packages #
+      ############
+      # it's better to unpack these files and commit the raw source
+      # git has its own built in compression methods
+      "*.7z"
+      "*.dmg"
+      "*.gz"
+      "*.iso"
+      "*.jar"
+      "*.rar"
+      "*.tar"
+      "*.zip"
+
+      # Logs and databases #
+      ######################
+      "*.log"
+      "*.sqlite"
+
+      # OS generated files #
+      ######################
+      ".DS_Store"
+      ".DS_Store?"
+      "._*"
+      ".Spotlight-V100"
+      ".Trashes"
+      "Icon?"
+      "ehthumbs.db"
+      "Thumbs.db"
+      ".*~"
+      "*.swp"
+      ".issues-cache"
+      ".bundle"
+      "node_modules/"
+      ".netrwhist"
+      "*.bak"
+      "tmp/"
+      ".vscode/"
+      "*.patch"
+      ".envrc"
+      ".kubeconfig"
+    ];
+  };
+}
