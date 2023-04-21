a6985a18d6e1a2a4d1afb2958d2e5e323dbeac98
Manuel Hutter <manuel@hutter.io>
2023-04-21 07:51:04 +0200

    Install cargo-watch, cargo-nextest
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- modules/rust.nix
+++ modules/rust.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ rustup ];
+  home.packages = with pkgs; [ rustup cargo-watch cargo-nextest ];
 
   home.file = {
     ".cargo/config.toml".text = ''
