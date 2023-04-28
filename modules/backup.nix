3d58bd7fd087ffcc2ce3b99c8d55199daa53bec4
Manuel Hutter <manuel@hutter.io>
2023-04-28 18:09:33 +0200

    Restic: Add cleanup job
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- modules/backup.nix
+++ modules/backup.nix
@@ -1,53 +1,69 @@
 { pkgs, config, ... }:
 let
   home = config.home.homeDirectory;
+
   excludes = builtins.toFile "restic-excludes" ''
     .dropbox-dist
     Dropbox/
     target/
   '';
+
+  cron = { name, calendar, command, randomDelay ? 60 * 60 }: {
+    services."${name}" = {
+      Service = {
+        Type = "oneshot";
+        Environment = [
+          "RESTIC_REPOSITORY=/mnt/backup/tera.restic"
+          "RESTIC_PASSWORD_FILE=${home}/.secrets/restic-password"
+        ];
+        ExecStart = builtins.concatStringsSep " " command;
+        Nice = 10;
+      };
+    };
+    timers."${name}" = {
+      Timer = {
+        OnCalendar = calendar;
+        Persistent = true;
+        # Randomize start time
+        RandomizedDelaySec = randomDelay;
+        # Always use the same random delay
+        FixedRandomDelay = true;
+      };
+      Install = {
+        WantedBy = [ "timers.target" ];
+      };
+    };
+  };
+
 in
 {
   home.packages = [ pkgs.restic ];
 
-  systemd.user.services."restic-backup" = {
-    Unit = {
-      Description = "Restic backup";
-    };
-    Service = {
-      Type = "oneshot";
-      Environment = [
-        "RESTIC_REPOSITORY=/mnt/backup/tera.restic"
-        "RESTIC_PASSWORD_FILE=${home}/.secrets/restic-password"
-      ];
-      ExecStart = builtins.concatStringsSep " " [
+  systemd.user = pkgs.lib.recursiveUpdate
+    (cron {
+      name = "restic-backup";
+      calendar = "hourly";
+      command = [
         "${pkgs.restic}/bin/restic backup"
-        "--verbose"
-        "--no-scan"
         "--compression=max"
-        "${home}"
-        "--tag=home"
-        "--tag=tera"
         "--exclude-file=${excludes}"
+        "--no-scan"
+        "--one-file-system"
+        "--verbose"
+        "${home}"
       ];
-      Nice = 10;
-    };
-  };
-  systemd.user.timers."restic-backup" = {
-    Unit = {
-      Description = "Regular restic backup";
-    };
-    Timer = {
-      OnCalendar = "hourly";
-      Unit = "restic-backup.service";
-      Persistent = true;
-      # Randomize start time
-      RandomizedDelaySec = 60 * 60;
-      # Always use the same random delay
-      FixedRandomDelay = true;
-    };
-    Install = {
-      WantedBy = [ "timers.target" ];
-    };
-  };
+    })
+    (cron {
+      name = "restic-cleanup";
+      calendar = "*-*-* 12:30:00";
+      command = [
+        "${pkgs.restic}/bin/restic forget"
+        "--keep-hourly=24"
+        "--keep-daily=7"
+        "--keep-weekly=4"
+        "--keep-monthly=12"
+        "--keep-yearly=2"
+      ];
+    });
+
 }
