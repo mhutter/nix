260782c1c9afec40a98309d8a5f16e8b6aa83364
Manuel Hutter <manuel@hutter.io>
2023-08-15 14:51:39 +0200

    update-nix-stuff: Disable store permission fix
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- bin/update-nix-stuff.sh
+++ bin/update-nix-stuff.sh
@@ -30,8 +30,8 @@ home-manager expire-generations "-30 days"
 # log "Cleaning up nix store"
 # nix-collect-garbage --delete-older-than 30d
 
-log "Fix Nix store permissions"
-sudo chmod -R -w /nix/store
+# log "Fix Nix store permissions"
+# sudo chmod -R -w /nix/store
 
 log "Optimize Nix store"
 nix store optimise
