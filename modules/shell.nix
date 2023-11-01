1d3f3c1bbfe25ff158264ee73701f5ae94f38d4c
Manuel Hutter <manuel@hutter.io>
2023-11-01 14:05:35 +0100

    Add dark/light switcher
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- modules/shell.nix
+++ modules/shell.nix
@@ -63,6 +63,9 @@ in
         cd "$p"
       }
 
+      alias dark="sed -i 's/gtk-application-prefer-dark-theme=./gtk-application-prefer-dark-theme=1/' ~/.config/gtk-3.0/settings.ini"
+      alias light="sed -i 's/gtk-application-prefer-dark-theme=./gtk-application-prefer-dark-theme=0/' ~/.config/gtk-3.0/settings.ini"
+
       ${fortune}/bin/strfile -c '%' -s ${cookies} ${cookies}.dat
       ${fortune}/bin/fortune ${cookies}
     '';
