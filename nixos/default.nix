24defd76f9544b7c6f16ad7884b0aa7f9c13d815
Manuel Hutter <manuel@hutter.io>
2024-08-23 20:13:39 +0200

    nixos: Fix console resolution
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- nixos/default.nix
+++ nixos/default.nix
@@ -7,6 +7,7 @@ in
 {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.consoleMode = "max";
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.networkmanager.enable = true;
