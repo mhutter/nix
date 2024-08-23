b37840d9a20a00a82e09108fb62c0b4e1487b808
Manuel Hutter <manuel@hutter.io>
2024-08-23 10:06:09 +0200

    fix: Ensure profile changes are displayed the right way around
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- home.nix
+++ home.nix
@@ -138,6 +138,6 @@ in
 
   home.activation.diff = ''
     echo === Profile changes ===
-    ${pkgs.home-manager}/bin/home-manager generations | head -n2 | ${pkgs.gawk}/bin/awk '{print $7}' | xargs nix store diff-closures
+    ${pkgs.home-manager}/bin/home-manager generations | head -n2 | sort | ${pkgs.gawk}/bin/awk '{print $7}' | xargs nix store diff-closures
   '';
 }
