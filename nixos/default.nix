832479890e2b1dc50d4f375ef84708649a560b8f
Manuel Hutter <manuel@hutter.io>
2024-09-07 14:49:52 +0200

    nixos: Enable sound
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- nixos/default.nix
+++ nixos/default.nix
@@ -29,10 +29,12 @@ in
   # Enable sound.
   # hardware.pulseaudio.enable = true;
   # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+  security.rtkit.enable = true;
 
   users.mutableUsers = false;
   users.users.${username} = {
