cd6c835b3bcd541f0e812a73698ff3d0d4799c5b
Manuel Hutter <manuel@hutter.io>
2024-09-08 12:15:18 +0200

    nxzt: Configure alacritty
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- hosts/nxzt/home.nix
+++ hosts/nxzt/home.nix
@@ -3,4 +3,51 @@
   imports = [
     ../../home
   ];
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      cursor.style.blinking = "Always";
+
+      font = {
+        size = 11;
+        normal.family = "DejaVuSansM Nerd Font";
+      };
+
+      colors = {
+        primary = {
+          background = "#1b2b34";
+          foreground = "#c0c5ce";
+        };
+
+        normal = {
+          black = "#1b2b34";
+          blue = "#6699cc";
+          cyan = "#5fb3b3";
+          green = "#99c794";
+          magenta = "#c594c5";
+          red = "#ec5f67";
+          white = "#c0c5ce";
+          yellow = "#fac863";
+        };
+
+        bright = {
+          black = "#65737e";
+          blue = "#6699cc";
+          cyan = "#5fb3b3";
+          green = "#99c794";
+          magenta = "#c594c5";
+          red = "#ec5f67";
+          white = "#d8dee9";
+          yellow = "#fac863";
+        };
+
+        cursor = {
+          cursor = "#c0c5ce";
+          text = "#1b2b34";
+        };
+
+      };
+    };
+  };
 }
