1f5a7fd878021dd6f190f36de54a56843c88249a
Manuel Hutter <manuel@hutter.io>
2025-04-01 05:59:53 +0200

    feat(nixos/notebook): Enable setting screen brightness
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- nixos/notebook.nix
+++ nixos/notebook.nix
@@ -1,12 +1,19 @@
 # Configuration specific to mobile workstations
-{ pkgs, ... }:
+{ pkgs, username, ... }:
 
 {
+  environment.systemPackages = [ pkgs.arandr ];
+
+  programs.light = {
+    enable = true;
+    brightnessKeys.enable = true;
+  };
+  users.users.${username}.extraGroups = [ "video" ];
+
   services.autorandr = {
     enable = true;
     hooks.postswitch = {
       "set-background" = "${pkgs.feh}/bin/feh --bg-fill ${../home/wallpaper.png}";
     };
   };
-  environment.systemPackages = [ pkgs.arandr ];
 }
