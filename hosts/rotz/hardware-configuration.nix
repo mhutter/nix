8696ee332db5ecb587430db1d8652c9ee3ae6c6a
Manuel Hutter <manuel@hutter.io>
2025-04-02 22:13:40 +0200

    rotz: Increase root disk size
    More RAM, more fun!
    
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- hosts/rotz/hardware-configuration.nix
+++ hosts/rotz/hardware-configuration.nix
@@ -14,7 +14,7 @@
   fileSystems."/" = {
     device = "none";
     fsType = "tmpfs";
-    options = [ "defaults" "size=8G" "mode=755" ];
+    options = [ "defaults" "size=16G" "mode=755" ];
   };
 
   fileSystems."/boot" = {
