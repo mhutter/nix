41145b6da71aa1162793a993dc7da02c427a2b30
Manuel Hutter <manuel@hutter.io>
2025-11-24 10:37:27 +0100

    rotz: Disable manual wg interfaces
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- hosts/rotz/default.nix
+++ hosts/rotz/default.nix
@@ -2,7 +2,6 @@
   pkgs,
   pkgs-citrix-workspace,
   username,
-  secrets,
   ...
 }:
 
@@ -17,23 +16,6 @@
 
   networking = {
     hostName = "rotz";
-    wireguard.enable = true;
-    wireguard.interfaces.wg0 =
-      let
-        wgdir = "/nix/persist/var/lib/wireguard";
-      in
-      {
-        privateKeyFile = "${wgdir}/private";
-        ips = [ "10.13.37.10/24" ];
-        peers = [
-          {
-            name = "bastion";
-            allowedIPs = [ "10.13.37.0/24" ];
-            presharedKeyFile = "${wgdir}/presharedkey";
-            inherit (secrets.wg) endpoint publicKey;
-          }
-        ];
-      };
   };
 
   environment.systemPackages = with pkgs; [
