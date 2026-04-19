b5aa6808e800f2044f333343198835365cec6f0a
Manuel Hutter <manuel@hutter.io>
2026-04-19 19:55:01 +0200

    [Maintenance]
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- home/modules/vim.nix
+++ home/modules/vim.nix
@@ -4,11 +4,17 @@
   programs.neovim = {
     enable = true;
 
+    # Prevent HM module from trying to overwrite ~/.config/nvim/init.lua
+    sideloadInitLua = true;
+
     defaultEditor = true;
     vimAlias = true;
     vimdiffAlias = true;
 
     withNodeJs = true;
+    withPython3 = false;
+    withRuby = false;
+
     # For whatever reason, the above does not make NodeJS available to LSP servers
     extraPackages = with pkgs; [
       # Plugin runtimes
